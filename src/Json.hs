f40d2c3752509ca62b2dbc72b38b9b9fc994c0e3
Leon P Smith <leon@melding-monads.com>
2011-06-02 00:54:23 -0400

    Code cleanups and optimize escaping
      1. instance Value a => Value (Maybe a)
         was removed because it was not injective.
      2. Code formatting improvements
      3. optimize \u00?? escapes

--- src/Json.hs
+++ src/Json.hs
@@ -11,6 +11,7 @@
 -----------------------------------------------------------------------------
 
 {-# LANGUAGE BangPatterns         #-}
+{-# LANGUAGE ViewPatterns         #-}
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE OverlappingInstances #-}
@@ -25,24 +26,28 @@ module Json
      ) where
 
 import           Blaze.ByteString.Builder as Blaze
-import           Data.Monoid
 import           Blaze.ByteString.Builder.ByteString
 import           Blaze.ByteString.Builder.Char8
+-- import           Blaze.ByteString.Builder.Char.Utf8
+import           Blaze.Text (float, double, integral)
+
+import           Data.Bits (shiftL, shiftR, (.&.))
+import qualified Data.Map               as Map
+import           Data.Monoid
+import           Data.Word (Word16, Word8)
+
+import qualified Data.Char              as Char
+
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Lazy   as BL
 import qualified Data.ByteString.UTF8   as BU
+import qualified Data.ByteString.Lazy.UTF8 as BLU
 import           Data.ByteString.Char8()
 import           Data.ByteString.Internal (w2c, c2w)
-import qualified Data.Char              as Char
-import qualified Data.Map               as Map
-import           Data.Word (Word16, Word8)
-import           Data.Bits (shiftL, shiftR)
-import           Data.String (IsString)
+
 import           Data.Text (Text)
 import qualified Data.Text              as T
 import           Data.Text.Encoding (encodeUtf8)
-import           Blaze.Text (float, double, integral)
-
 
 ---- The "core" of json-builder
 
@@ -55,24 +60,29 @@ class Value a where
 
 class Value a => Key a
 
-data Pair = Pair !Blaze.Builder !Bool
 
 newtype Object = Object (Bool -> Pair)
 
+data Pair = Pair !Blaze.Builder !Bool
+
 instance Value Object where
-  toBuilder (Object f) = case f True of
-                           Pair fb _ -> mconcat [fromChar '{', fb, fromChar '}']
+  toBuilder (Object f)
+    = case f True of
+        Pair fb _ -> mconcat [fromChar '{', fb, fromChar '}']
 
 instance Monoid Object where
   mempty = Object $ \x -> Pair mempty x
   mappend (Object f) (Object g)
-      = Object $ \x -> case f x of
-                         Pair fb x' -> case g x' of
-                                         Pair gb x'' -> Pair (fb `mappend` gb) x''
+    = Object $ \x -> case f x of
+                      Pair fb x' ->
+                           case g x' of
+                            Pair gb x'' ->
+                                 Pair (fb `mappend` gb) x''
 
 row :: (Key a, Value b) => a -> b -> Object
-row str a = Object $ comma (mconcat [ toBuilder str, fromChar ':',  toBuilder a ])
+row a b = Object syntax
   where
+    syntax = comma (mconcat [ toBuilder a, fromChar ':',  toBuilder b ])
     comma b True  = Pair b False
     comma b False = Pair (fromChar ',' `mappend` b) False
 
@@ -80,15 +90,18 @@ row str a = Object $ comma (mconcat [ toBuilder str, fromChar ':',  toBuilder a 
 newtype Array = Array (Bool -> Pair)
 
 instance Value Array where
-  toBuilder (Array f) = case f True of
-                          Pair fb _ -> mconcat [fromChar '[', fb, fromChar ']']
+  toBuilder (Array f)
+    = case f True of
+        Pair fb _ -> mconcat [fromChar '[', fb, fromChar ']']
 
 instance Monoid Array where
   mempty = Array $ \x -> Pair mempty x
   mappend (Array f) (Array g)
-      = Array $ \x -> case f x of
-                         Pair fb x' -> case g x' of
-                                         Pair gb x'' -> Pair (fb `mappend` gb) x''
+    = Array $ \x -> case f x of
+                     Pair fb x' ->
+                          case g x' of
+                           Pair gb x'' ->
+                                Pair (fb `mappend` gb) x''
 
 element :: Value a => a -> Array
 element a = Array $ comma (toBuilder a)
@@ -116,48 +129,35 @@ instance Value Bool where
   toBuilder False = fromByteString "false"
 
 instance Value BS.ByteString where
-  toBuilder x = fromWrite (writeChar '"' `mappend` loop (splitQ x))
+  toBuilder x = fromChar '"' `mappend` loop (splitQ x)
     where
-      loop (a,b)
-        = writeByteString a `mappend`
-            case BU.decode b of
-              Nothing     ->  writeChar '"'
-              Just (c,n)  ->  writeByteString (quoteChar c) `mappend`
-                                loop (splitQ (BS.drop n b))
-
       splitQ = BU.break quoteNeeded
 
-      quoteNeeded :: Char -> Bool
-      quoteNeeded c = c == '\\' || c == '"' || Char.ord c < 0x20
-
-      quoteChar :: Char -> BS.ByteString
-      quoteChar c = case c of
-                     '\\'  ->  "\\\\"
-                     '"'   ->  "\\\""
-                     '\b'  ->  "\\b"
-                     '\f'  ->  "\\f"
-                     '\n'  ->  "\\n"
-                     '\r'  ->  "\\r"
-                     '\t'  ->  "\\t"
-                     _     ->  hexEscape c
-
-      hexEscape :: Char -> BS.ByteString
-      hexEscape c = s
-        where
-          !n = fromIntegral (Char.ord c) :: Word16
-
-          !s = BS.append "\\u" $ fst $ BS.unfoldrN 4 f n
-
-          f n = Just (char (fromIntegral (n `shiftR` 12)), n `shiftL` 4)
-
-          char i
-            | i < 10    = (c2w '0' -  0) + i
-            | otherwise = (c2w 'a' - 10) + i
+      loop (a,b)
+        = fromByteString a `mappend`
+            case BU.decode b of
+              Nothing     ->  fromChar '"'
+              Just (c,n)  ->  fromWrite (quoteChar c) `mappend`
+                                loop (splitQ (BS.drop n b))
 
 instance Key BS.ByteString
 
+instance Value BL.ByteString where
+  toBuilder x = fromChar '"' `mappend` loop (splitQ x)
+    where
+      splitQ = BLU.break quoteNeeded
+
+      loop (a,b)
+        = fromLazyByteString a `mappend`
+            case BLU.decode b of
+              Nothing     ->  fromChar '"'
+              Just (c,n)  ->  fromWrite (quoteChar c) `mappend`
+                                loop (splitQ (BL.drop n b))
+
+instance Key BL.ByteString
+
 -- FIXME: rewrite/optimize the quoting routines for ByteString, Text, String
---        Should we support direct quoting of lazy strings?
+--        Should we support direct quoting of lazy ByteStrings/ lazy Text?
 
 instance Value Text where
   toBuilder = toBuilder . encodeUtf8
@@ -171,12 +171,37 @@ instance Key [Char]
 
 -- Convenient (?) instances for json-builder
 
-instance Value a => Value (Maybe a) where
-  toBuilder Nothing  = fromByteString "null"
-  toBuilder (Just a) = toBuilder a
-
 instance Value a => Value [a] where
   toBuilder = toBuilder . mconcat . map element
 
 instance Value a => Value (Map.Map BS.ByteString a) where
-  toBuilder = toBuilder . Map.foldrWithKey (\k a b -> row k a `mappend` b) mempty
+  toBuilder = toBuilder
+            . Map.foldrWithKey (\k a b -> row k a `mappend` b) mempty
+
+
+------------------------------------------------------------------------------
+
+quoteNeeded :: Char -> Bool
+quoteNeeded c = c == '\\' || c == '"' || Char.ord c < 0x20
+
+quoteChar :: Char -> Write
+quoteChar c = case c of
+                '\\'  ->  writeByteString "\\\\"
+                '"'   ->  writeByteString "\\\""
+                '\b'  ->  writeByteString "\\b"
+                '\f'  ->  writeByteString "\\f"
+                '\n'  ->  writeByteString "\\n"
+                '\r'  ->  writeByteString "\\r"
+                '\t'  ->  writeByteString "\\t"
+                _     ->  hexEscape c
+
+hexEscape  :: Char -> Write
+hexEscape  (c2w -> c)
+  = writeByteString "\\u00"
+    `mappend` writeWord8 (char ((c `shiftR`  4) .&. 0xF))
+    `mappend` writeWord8 (char ( c              .&. 0xF))
+
+char :: Word8 -> Word8
+char i | i < 10    = i + 48
+       | otherwise = i + 87
+{-# INLINE char #-}
