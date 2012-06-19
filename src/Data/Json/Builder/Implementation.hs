2a8bb3b8b97bc1c26034bf03901d456de4cbcfa1
Leon P Smith <leon@melding-monads.com>
2012-06-19 06:19:39 -0400

    Update copyrights

--- src/Data/Json/Builder/Implementation.hs
+++ src/Data/Json/Builder/Implementation.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Json.Builder.Implementation
--- Copyright   :  (c) 2011 Leon P Smith
+-- Copyright   :  (c) 2011-2012 Leon P Smith
 -- License     :  BSD3
 --
 -- Maintainer  :  Leon P Smith <leon@melding-monads.com>
